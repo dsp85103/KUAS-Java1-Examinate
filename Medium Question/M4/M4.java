f49b1f3fb4bc1358f3271d077f155b65d01121ec
dsp85103 <dsp85103@gmail.com>
2017-01-06 10:47:53 +0800

    拿掉無用的變數(one)

--- Medium Question/M4/M4.java
+++ Medium Question/M4/M4.java
@@ -3,13 +3,13 @@ public class M4 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		System.out.print("輸入票價P: ");
-		int ten=0,five=0,one=0,price = scn.nextInt();
+		int ten=0,five=0,price = scn.nextInt();
 		if(price>=0 && price<=1000) {
-			while(price/10!=0) {
+			if(price/10!=0) {
 				ten=price/10;
 				price = price%10;
 			}
-			while(price/5!=0) {
+			if(price/5!=0) {
 				five=price/5;
 				price = price%5;
 			}
