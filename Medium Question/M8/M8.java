dccf1cfd76ac0da981bb7d8fc9471948860416d6
dsp85103 <dsp85103@gmail.com>
2017-01-06 11:39:40 +0800

    修改 字母判斷方式&判斷真假順序

--- Medium Question/M8/M8.java
+++ Medium Question/M8/M8.java
@@ -4,27 +4,28 @@ public class M8 {
         Scanner scn = new Scanner(System.in);
         System.out.print("輸入身分證字號 : ");
         String id = scn.nextLine();
-        char tmp = id.charAt(0);
         int authValue=0;
-        if((int)tmp>=65 && (int)tmp<=90) { //A~Z
-            if((int)tmp>=65 && (int)tmp<=72) //A~H
-                authValue = ((int)tmp-55)/10 + (((int)tmp-55)%10)*9; 
-            if((int)tmp>=74 && (int)tmp<=78) //J~N
-                authValue = ((int)tmp-56)/10 + (((int)tmp-56)%10)*9;
-            if((int)tmp>=80 && (int)tmp<=86) //P~V
-                authValue = ((int)tmp-57)/10 + (((int)tmp-57)%10)*9;
-            if((int)tmp>=88 && (int)tmp<=90) //X~Z
-                authValue = ((int)tmp-58)/10 + (((int)tmp-58)%10)*9;
-            if((int)tmp==73) authValue = ((int)tmp-39)/10 + (((int)tmp-39)%10)*9; // I
-            if((int)tmp==79) authValue = ((int)tmp-44)/10 + (((int)tmp-44)%10)*9; // O
-        } 
-        for(int i=0;i<9;i++) {
-            if(i==8) 
-                authValue += (int)id.charAt(i+1)-48;
-            else
-                authValue += ((int)id.charAt(i+1)-48)*(8-i);
-        }
-        if(authValue%10==0) 
-            System.out.println("此身分證號碼為 真!");
+		String x[] = {"A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","X","Y","W","Z","I","O"};
+        String y[] = {"10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35"};
+        if(!id.substring(1, 2).equals("1") && !id.substring(1, 2).equals("2")){
+			System.out.println("此身分證字號為 假!");
+		}else{
+			for(int i=0;i<x.length;i++) {
+				if(id.substring(0,1).equals(x[i])) {
+					id = id.replace(x[i],y[i]);
+					break;
+				}
+			}
+			for(int i=0;i<11;i++) {
+				if(i==0 || i==10) 
+					authValue += (int)id.charAt(i)-48;
+				else
+					authValue += ((int)id.charAt(i)-48)*(10-i);
+			}
+			if(authValue%10==0) 
+				System.out.println("此身分證號碼為 真!");
+			else
+				System.out.println("此身分證字號為 假!");
+		}
     }
 }
